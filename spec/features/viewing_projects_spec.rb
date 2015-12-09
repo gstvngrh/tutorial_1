7f86eb94eede2a6691063c2df78359b698ec6dd9
gstvngrh <gustav.nugraha@gmail.com>
2015-12-09 23:46:28 +0700

    Projects can now be updated

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 RSpec.feature "Users can view projects" do
   scenario "with the project details" do
-    
+    project = FactoryGirl.create(:project, name: "Atom")
+    visit "/"
+    click_link "Atom"
+    expect(page.current_url).to eq project_url(project)
   end
 end
