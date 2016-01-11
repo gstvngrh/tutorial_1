41bd04679d44132a6acf06092daeb9de25424e46
gstvngrh <gustav.nugraha@gmail.com>
2016-01-11 22:54:22 +0700

    Only editors, managers and admins can create tickets

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -11,31 +11,37 @@ describe TicketPolicy do
     context "for anonymous users" do
       let(:user) { nil }
       it { should_not permit_action :show }
+      it { should_not permit_action :create }
     end
 
     context "for viewers for the project" do
       before { assign_role!(user, :viewer, project) }
       it { should permit_action :show }
+      it { should_not permit_action :create }
     end
 
     context "for editors for the project" do
       before { assign_role!(user, :editor, project) }
       it { should permit_action :show }
+      it { should permit_action :create }
     end
 
     context "for managers for the project" do
       before { assign_role!(user, :manager, project) }
       it { should permit_action :show }
+      it { should permit_action :create }
     end
 
     context "for managers of other projects" do
       before { assign_role!(user, :manager, FactoryGirl.create(:project)) }
       it { should_not permit_action :show }
+      it { should_not permit_action :create }
     end
 
     context "for administrators" do
       let(:user) { FactoryGirl.create(:user, :admin) }
       it { should permit_action :show }
+      it { should permit_action :create }
     end
   end
 end
